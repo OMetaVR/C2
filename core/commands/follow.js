56a0be60afbc6cc0732ba78ba1bc5bea4d4162d3
Takkeshi <prosaso12133@gmail.com>
2023-10-08 11:49:57 -0600

    Made follow function (not tested)

--- core/commands/follow.js
+++ core/commands/follow.js
@@ -2,6 +2,46 @@
 const mineflayer = require('mineflayer');
 const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 
-//Load pathfinder plugin
-bot.loadPlugin(pathfinder);
+class Follow{
+    constructor(bot, username){
+        this.bot = bot;
+        this.username = username;
+        this.followInterval = null;
+        bot.loadPlugin(pathfinder);
+    }
 
+    followPlayer(username){
+        const player = bot.players[username];
+
+        if(!player){
+            //Send player not found error on console
+            return;
+        }
+
+        bot.pathfinder.setMovements(new Movements(bot, player));
+        bot.pathfinder.setGoal(new goals.GoalNear(player.position.x, player.position.y, player.position.z, 1));
+
+        this.followInterval = setInterval(() => {
+            if(!player.entity){
+                //Same as above
+                StopFollow(bot);
+                return;
+            }
+            bot.pathfinder.setMovements(new Movements(bot, player));
+            bot.pathfinder.setGoal(new goals.GoalNear(player.position.x, player.position.y, player.position.z, 1));
+        }, 500);
+        
+    }
+}
+
+class StopFollow{
+    constructor(bot){
+        this.bot = bot;
+    }
+
+    stopFollowing(){
+        //Send some stop logs too, maybe add a dev option to send ALL posible logs, just for info.
+        clearInterval(this.followInterval);
+        bot.pathfinder.setGoal(null);
+    }
+}
