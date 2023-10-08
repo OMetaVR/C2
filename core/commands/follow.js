c14a22a99e23738e6643989fcbcc30652ab1f2f1
Takkeshi <prosaso12133@gmail.com>
2023-10-08 01:11:43 -0600

    stuff

---
+++ core/commands/follow.js
@@ -0,0 +1,7 @@
+//Main library
+const mineflayer = require('mineflayer');
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
+
+//Load pathfinder plugin
+bot.loadPlugin(pathfinder);
+
